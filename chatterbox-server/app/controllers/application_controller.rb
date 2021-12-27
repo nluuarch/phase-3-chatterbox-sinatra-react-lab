80f5e6cfbe275465a0c2283dccf3fed8d78b1d1a
nluuarch@gmail.com <nluuarch@gmail.com>
2021-12-26 19:35:39 -0800

    Completed Assignment

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,33 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  get '/messages' do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username],
+      created_at: params[:created_at],
+      updated_at: params[:updated_at]
+    )
+    message.to_json
+  end
+
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+    message.to_json
+  end
   
 end
